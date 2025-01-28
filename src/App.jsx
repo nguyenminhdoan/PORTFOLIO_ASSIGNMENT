c44259611380b3109494e139094cdd6cc2eb6256
nguyenminhdoan <nguyenminhdoan0303@gmail.com>
2025-01-28 16:22:30 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import WelcomeScreen from "./Pages/WelcomeScreen";
+import { AnimatePresence } from "framer-motion";
+import Home from "./Pages/Home";
+
+const LandingPage = ({ showWelcome, setShowWelcome }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/home");
+    }, 6000);
+  }, []);
+
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {showWelcome && (
+          <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
+        )}
+      </AnimatePresence>
+
+      {!showWelcome && (
+        <>
+          <footer>
+            <center>
+              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                © 2023{" "}
+                <a href="https://flowbite.com/" className="hover:underline">
+                  MD
+                </a>
+                . All Rights Reserved.
+              </span>
+            </center>
+          </footer>
+        </>
+      )}
+    </>
+  );
+};
+
+const ProjectPageLayout = () => (
+  <>
+    <ProjectDetails />
+    <footer>
+      <center>
+        <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+        <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+          © 2023{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            MD
+          </a>
+          . All Rights Reserved.
+        </span>
+      </center>
+    </footer>
+  </>
+);
+
+function App() {
+  const [showWelcome, setShowWelcome] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("isWelcome") != 1) {
+      setShowWelcome(true);
+      localStorage.setItem("isWelcome", 1);
+    }
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LandingPage
+              showWelcome={showWelcome}
+              setShowWelcome={setShowWelcome}
+            />
+          }
+        />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
